30d0ac32cde61c4fcc2eeba778abd5c954b1d037
usurinisso <tautvis477@gmail.com>
2020-07-08 18:47:12 +0300

    Added simple IBAN file validation controller

---
+++ demo/src/main/java/seb/iban/controller/FileValidationController.java
@@ -0,0 +1,16 @@
+package seb.iban.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.servlet.ModelAndView;
+
+@Controller
+public class FileValidationController {
+    @RequestMapping( value = {"/upload"}, method = RequestMethod.GET )
+    public ModelAndView validate() {
+        ModelAndView modelAndView = new ModelAndView();
+        modelAndView.setViewName("upload");
+        return modelAndView;
+    }
+}
