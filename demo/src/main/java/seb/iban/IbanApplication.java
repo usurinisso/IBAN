ed99ddb3ec1bf992b4b3e21f806329b7fa2c7e44
usurinisso <tautvis477@gmail.com>
2020-07-08 18:47:50 +0300

    Spring boot aplication

---
+++ demo/src/main/java/seb/iban/IbanApplication.java
@@ -0,0 +1,12 @@
+package seb.iban;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IbanApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(IbanApplication.class, args);
+    }
+}
